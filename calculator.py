dd08251fe9424ad40153c3f4393faa62ced0f4bb
rameshr85022-ops <rameshr85022@gmail.com>
2026-09-18 15:29:20 +0530

    Implement basic calculator for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,6 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+addition = a + b
+subtraction = a - b
+print("Addition =", addition)
+print("Subtraction =", subtraction)
